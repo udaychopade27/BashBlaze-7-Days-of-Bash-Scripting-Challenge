c913b486f0bba9fea5fbd8709405cf1713097d25
udaychopade27 <133924961+udaychopade27@users.noreply.github.com>
2023-07-31 13:15:47 +0530

    Add files via upload
    Day 1 Challenge script file uploaded to github

--- Challenges/solution_day1_script.sh
+++ Challenges/solution_day1_script.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-
 # First line of the script is the shebang which tells the system how to execute
 
 # Task  2: Echo
-echo "Scripting is fun with @TWS"
+echo "Hello Bash. it's fun to complete this challenge."
 
 # Task  3: Variables
-variable1="Hello"
-variable2="Bash"
+variable1=5
+variable2=9
 
 # Task  4: Using Variables
-greeting="$variable1, $variable2!"
-echo "$greeting Welcome to the world of Bash scripting!"
+sum=$(($variable1 + $variable2))
+echo "The sum of $variable1 and $variable2 is $sum"
 
 # Task  5: Using Built-in Variables
 echo "My current bash path - $BASH"
@@ -20,12 +19,10 @@ echo "PID of bash I am running - $$"
 echo "My home directory - $HOME"
 echo "Where am I currently? - $PWD"
 echo "My hostname - $HOSTNAME"
+echo "The File editor is- $EDITOR"
+echo "The user ID of current user is - $UID"
+echo "Tjhe location of current user's shell program is - $SHELL"
 
 # Task  6: Wildcards
 echo "Files with .txt extension in the current directory:"
-ls *.txt
-
-
-
-#Make sure to provide execution permission with the following command:
-#chmod +x day1_script.sh
+ls *.md
