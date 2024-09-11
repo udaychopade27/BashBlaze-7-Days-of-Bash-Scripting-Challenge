99e2084b69f49e31186a31fd3229fec2f19a8622
udaychopade27 <udaychopade27@gmail.com>
2024-09-11 09:04:02 +0530

    added solution for day_1 Problem statement

---
+++ Challenges/Day_1/Solution_Day_1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#Task 1
+# This script is written for a 7 days challenges to learn bash scripting
+
+<<comment
+There are several problem statement are given in a repository and we have to add solution for all the problems
+In saperate Folder for saperate day
+comment
+
+#Task 2
+echo "Hello Everyone, I have taken  7 days Bash scripting Challenge to learn scripting"
+
+
+#Task 3
+
+var1="Hello"
+var2="Everyone"
+
+#Task 4
+greeting="$var1, $var2 !!"
+
+echo "$greeting, Welcome to the challenge of Bash scripting"
+
+#Task 5
+
+echo "My Current working Path is $PWD"
+echo "My home directory - $HOME"
+echo "Where am I currently? - $PWD"
+echo "My hostname - $HOSTNAME"
+
+#Task 6
+echo "Listing all .txt files in current Directory"
+ls *.txt
+echo "--------------------------"
+echo "Listing all .sh files in current Directory"
+ls *.sh
+
+
+
+
